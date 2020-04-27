960227dc6ab79c7926a1390ee0684433b402a614
Jez Higgins <jez@jezuk.co.uk>
2020-04-27 14:26:20 +0100

    test: Setup in before(), teardown in after()
    Grouped tests with describes()

--- test/test.js
+++ test/test.js
@@ -52,7 +52,7 @@ describe('Run some tests', function () {
     }
   })
 
-  it('Should setup database connection', (done) => {
+  before('Should setup database connection', (done) => {
     client = new HlPgClient(process.env.PG_CONNECTION_STRING)
     done()
   })
@@ -61,132 +61,138 @@ describe('Run some tests', function () {
     return client.runFile(path.resolve(__dirname, 'fixtures', 'scripts', 'setup.sql'))
   })
 
-  it('Should test the statement to generate match table', (done) => {
-    const statement = generateStatementInitTable(options)
-    expect(statement.trim()).to.eql('CREATE SCHEMA IF NOT EXISTS match_test_results; ' +
-      'DROP TABLE IF EXISTS match_test_results.food_addressbase; ' +
-      'CREATE TABLE match_test_results.food_addressbase ' +
-      '(food_id bigint NOT NULL PRIMARY KEY, address_id bigint, match_certainty integer);')
-    done()
+  describe('exact match', () => {
+    it('Should test processing the where part of a statement with both strings', (done) => {
+      const where = processWhere(
+        'exact',
+        ['full_name'],
+        ['first_name']
+      )
+      expect(where.trim()).to.eql('(' +
+        'upper(full_name) = upper(first_name))')
+      done()
+    })
+
+    it('Should test processing the where part of a statement with only source array', (done) => {
+      const where = processWhere(
+        'exact',
+        ['full_name', 'name'],
+        ['first_name']
+      )
+      expect(where.trim()).to.eql('(' +
+        'upper(full_name) = upper(first_name) OR ' +
+        'upper(name) = upper(first_name))')
+      done()
+    })
+
+    it('Should test processing the where part of a statement with only target array', (done) => {
+      const where = processWhere(
+        'exact',
+        ['full_name'],
+        ['first_name', 'last_name']
+      )
+      expect(where.trim()).to.eql('(' +
+        'upper(full_name) = upper(first_name) OR ' +
+        'upper(full_name) = upper(last_name))')
+      done()
+    })
+
+    it('Should test processing the where part of a statement with both arrays', (done) => {
+      const where = processWhere(
+        'exact',
+        ['full_name', 'name'],
+        ['first_name', 'middle_name']
+      )
+      expect(where.trim()).to.eql('(' +
+        'upper(full_name) = upper(first_name) OR ' +
+        'upper(full_name) = upper(middle_name) OR ' +
+        'upper(name) = upper(first_name) OR ' +
+        'upper(name) = upper(middle_name))')
+      done()
+    })
   })
 
-  it('Should test processing the where part of a statement with both strings', (done) => {
-    const where = processWhere(
-      'exact',
-      ['full_name'],
-      ['first_name']
-    )
-    expect(where.trim()).to.eql('(' +
-      'upper(full_name) = upper(first_name))')
-    done()
+  describe('fuzzy match', () => {
+    it('Should test processing the where part of a statement with only source array', (done) => {
+      const where = processWhere(
+        'fuzzy',
+        ['full_name', 'name'],
+        ['first_name']
+      )
+      expect(where.trim()).to.eql('(' +
+        'difference(full_name, first_name) = 4 OR ' +
+        'difference(name, first_name) = 4)')
+      done()
+    })
+
+    it('Should test processing the where part of a statement with only target array', (done) => {
+      const where = processWhere(
+        'fuzzy',
+        ['full_name'],
+        ['first_name', 'last_name']
+      )
+      expect(where.trim()).to.eql('(' +
+        'difference(full_name, first_name) = 4 OR ' +
+        'difference(full_name, last_name) = 4)')
+      done()
+    })
+
+    it('Should test processing the where part of a statement with both arrays', (done) => {
+      const where = processWhere(
+        'fuzzy',
+        ['full_name', 'name'],
+        ['first_name', 'middle_name']
+      )
+      expect(where.trim()).to.eql('(' +
+        'difference(full_name, first_name) = 4 OR ' +
+        'difference(full_name, middle_name) = 4 OR ' +
+        'difference(name, first_name) = 4 OR ' +
+        'difference(name, middle_name) = 4)')
+      done()
+    })
   })
 
-  it('Should test processing the where part of a statement with only source array', (done) => {
-    const where = processWhere(
-      'exact',
-      ['full_name', 'name'],
-      ['first_name']
-    )
-    expect(where.trim()).to.eql('(' +
-      'upper(full_name) = upper(first_name) OR ' +
-      'upper(name) = upper(first_name))')
-    done()
+  describe('match table', () => {
+    it('Should test the statement to generate match table', (done) => {
+      const statement = generateStatementInitTable(options)
+      expect(statement.trim()).to.eql('CREATE SCHEMA IF NOT EXISTS match_test_results; ' +
+        'DROP TABLE IF EXISTS match_test_results.food_addressbase; ' +
+        'CREATE TABLE match_test_results.food_addressbase ' +
+        '(food_id bigint NOT NULL PRIMARY KEY, address_id bigint, match_certainty integer);')
+      done()
+    })
+
+    it('Should match the tables', () => {
+      return matchTables(options, client)
+    })
+
+    it('Should check the results', async () => {
+      const results = await client.query(`select food_id, address_id from ${options.match.schema}.${options.match.table} where match_certainty != 0`)
+      expect(results.rows).to.eql([
+        { food_id: '111111', address_id: '111' },
+        { food_id: '987654', address_id: '987' },
+        { food_id: '444444', address_id: '444' },
+        { food_id: '555555', address_id: '555' },
+        { food_id: '666666', address_id: '666' },
+        { food_id: '777777', address_id: '777' },
+        { food_id: '888888', address_id: '888' }
+      ])
+    })
+
+    it('Should check the results', async () => {
+      const results = await client.query(`select food_id, address_id from ${options.match.schema}.${options.match.table} where match_certainty = 0`)
+      expect(results.rows).to.eql([
+        { food_id: '222222', address_id: null },
+        { food_id: '333333', address_id: null }
+      ])
+    })
   })
 
-  it('Should test processing the where part of a statement with only target array', (done) => {
-    const where = processWhere(
-      'exact',
-      ['full_name'],
-      ['first_name', 'last_name']
-    )
-    expect(where.trim()).to.eql('(' +
-      'upper(full_name) = upper(first_name) OR ' +
-      'upper(full_name) = upper(last_name))')
-    done()
-  })
-
-  it('Should test processing the where part of a statement with both arrays', (done) => {
-    const where = processWhere(
-      'exact',
-      ['full_name', 'name'],
-      ['first_name', 'middle_name']
-    )
-    expect(where.trim()).to.eql('(' +
-      'upper(full_name) = upper(first_name) OR ' +
-      'upper(full_name) = upper(middle_name) OR ' +
-      'upper(name) = upper(first_name) OR ' +
-      'upper(name) = upper(middle_name))')
-    done()
-  })
-
-  it('Should test processing the where part of a statement with only source array', (done) => {
-    const where = processWhere(
-      'fuzzy',
-      ['full_name', 'name'],
-      ['first_name']
-    )
-    expect(where.trim()).to.eql('(' +
-      'difference(full_name, first_name) = 4 OR ' +
-      'difference(name, first_name) = 4)')
-    done()
-  })
-
-  it('Should test processing the where part of a statement with only target array', (done) => {
-    const where = processWhere(
-      'fuzzy',
-      ['full_name'],
-      ['first_name', 'last_name']
-    )
-    expect(where.trim()).to.eql('(' +
-      'difference(full_name, first_name) = 4 OR ' +
-      'difference(full_name, last_name) = 4)')
-    done()
-  })
-
-  it('Should test processing the where part of a statement with both arrays', (done) => {
-    const where = processWhere(
-      'fuzzy',
-      ['full_name', 'name'],
-      ['first_name', 'middle_name']
-    )
-    expect(where.trim()).to.eql('(' +
-      'difference(full_name, first_name) = 4 OR ' +
-      'difference(full_name, middle_name) = 4 OR ' +
-      'difference(name, first_name) = 4 OR ' +
-      'difference(name, middle_name) = 4)')
-    done()
-  })
-
-  it('Should match the tables', () => {
-    return matchTables(options, client)
-  })
-
-  it('Should check the results', async () => {
-    const results = await client.query(`select food_id, address_id from ${options.match.schema}.${options.match.table} where match_certainty != 0`)
-    expect(results.rows).to.eql([
-      { food_id: '111111', address_id: '111' },
-      { food_id: '987654', address_id: '987' },
-      { food_id: '444444', address_id: '444' },
-      { food_id: '555555', address_id: '555' },
-      { food_id: '666666', address_id: '666' },
-      { food_id: '777777', address_id: '777' },
-      { food_id: '888888', address_id: '888' }
-    ])
-  })
-
-  it('Should check the results', async () => {
-    const results = await client.query(`select food_id, address_id from ${options.match.schema}.${options.match.table} where match_certainty = 0`)
-    expect(results.rows).to.eql([
-      { food_id: '222222', address_id: null },
-      { food_id: '333333', address_id: null }
-    ])
-  })
-
-  it('Should uninstall test schemas', () => {
+  after('Should uninstall test schemas', () => {
     return client.runFile(path.resolve(__dirname, 'fixtures', 'scripts', 'cleanup.sql'))
   })
 
-  it('Should close database connections', function (done) {
+  after('Should close database connections', function (done) {
     client.end()
     done()
   })
